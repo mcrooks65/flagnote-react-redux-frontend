5e0b670e9a2b30eb37f9ecddbd824adeffe5f018
mcrooks65 <mfcrooks@gmail.com>
2021-05-12 14:54:46 -0700

    Install redux related packages

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
 
 import App from './App';
+
+// need to set up our store
 
 ReactDOM.render(
   <React.StrictMode>
