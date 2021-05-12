508f648b7753ccae0317aebb876cb562b551058d
mcrooks65 <mfcrooks@gmail.com>
2021-05-12 14:20:20 -0700

    Test backend connection with fetch request

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import React from 'react';
 
 class App extends React.Component {
+
+  componentDidMount() {
+    fetch('http://localhost:3001/api/v1/engagements/1')
+    .then(response => response.json())
+    .then(response => console.log(response.data.attributes.targets[0]))
+  }  // Testing pulling targets out of engagement, routed by url /id# 
+
   render() {
     return (
       <div className="App">
