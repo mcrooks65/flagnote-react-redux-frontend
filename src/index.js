74177972044c7de190e3fd89bc0e77a58e84ed6a
mcrooks65 <mfcrooks@gmail.com>
2021-05-12 16:23:53 -0700

    Stub out engagementReducer.js

--- src/index.js
+++ src/index.js
@@ -3,17 +3,16 @@ import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
+import engagementReducer from './reducers/engagementReducer'
 
 import App from './App';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-let store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)))
+let store = createStore(engagementReducer, composeEnhancers(applyMiddleware(thunk)))
 
 ReactDOM.render(
   <Provider store={store}>
     <App />
-  </Provider>
-  ,
-  document.getElementById('root')
-);
+  </Provider>,
+  document.getElementById('root'));
---
+++ src/reducers/engagementReducer.js
@@ -0,0 +1,6 @@
+export default function engagementReducer(state = {engagements: []}, action) {
+
+    return state;
+
+}
+
