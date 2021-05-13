36364d7598cfa3911f493547945dce1f21c14970
mcrooks65 <mfcrooks@gmail.com>
2021-05-12 17:09:44 -0700

    Stub out fetchEngagements.js

---
+++ src/actions/fetchEngagements.js
@@ -0,0 +1,8 @@
+export function fetchEngagements(action) {
+    // componentDidMount() {
+    //     fetch('http://localhost:3001/api/v1/engagements') 
+    //     .then(response => response.json())
+    //     .then(data => console.log(data)
+    // }
+    return action
+}
