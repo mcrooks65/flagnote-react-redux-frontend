0655a500f934844e7fc5f940f51147ba0887d9f3
mcrooks65 <mfcrooks@gmail.com>
2021-05-18 13:35:28 -0700

    Update fetchEngagements.js

--- src/actions/fetchEngagements.js
+++ src/actions/fetchEngagements.js
@@ -1,8 +1,11 @@
-export function fetchEngagements(action) {
-    // componentDidMount() {
-    //     fetch('http://localhost:3001/api/v1/engagements') 
-    //     .then(response => response.json())
-    //     .then(data => console.log(data)
-    // }
-    return action
+export function fetchEngagements() {
+    return (dispatch) => {
+        fetch('http://localhost:3001/api/v1/engagements') 
+        .then(response => response.json())
+        .then(engagements => dispatch({
+            type: 'FETCH_ENGAGEMENTS',
+            payload: engagements
+        }))
+    }
 }
+
