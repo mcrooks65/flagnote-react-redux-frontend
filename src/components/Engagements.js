56363c51f1efa4189f4473c95ba159fe818ef8e6
mcrooks65 <mfcrooks@gmail.com>
2021-05-19 17:22:56 -0700

    Engagements.js fixed!

--- src/components/Engagements.js
+++ src/components/Engagements.js
@@ -1,12 +1,11 @@
 import React from 'react'
 
 const Engagements = (props) => {
-
+    
   return (
-
-    // When uncommented we have a TypeError: props.engagements.map is not a function - Need to find a fix! 
+    
     <div>
-      {/* {props.engagements.map(engagement => <li key={engagement.id}>{engagement.name} - {engagement.status}</li> )} */}
+        {props.engagements.map(engagement => <li key={engagement.id}>{engagement.name} - {engagement.status}</li> )}
     </div>
 
   )
