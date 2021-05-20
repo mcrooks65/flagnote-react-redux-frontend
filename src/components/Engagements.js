f320f04a4664dbae30f7c7783cbe0db254dbf44f
mcrooks65 <mfcrooks@gmail.com>
2021-05-20 14:59:29 -0700

    Update Engagements.js

--- src/components/Engagements.js
+++ src/components/Engagements.js
@@ -1,13 +1,36 @@
 import React from 'react'
 
 const Engagements = (props) => {
-    
+    console.log(props)
   return (
     
     <div>
-        {props.engagements.map(engagement => <li key={engagement.id}>{engagement.name} - {engagement.status}</li> )}
+        <h2>Engagements Listing</h2>
+        <ul>
+            {props.engagements.map(engagement => 
+                <li key={engagement.id}>
+                    <h3>#{engagement.id}. {engagement.name} - {engagement.status} </h3>
+                    <ul> 
+                        {engagement.targets.map(target => 
+                            <li key={target.id}>
+                                Hostname - {target.hostname}
+                                <ul>
+                                    <li>IP - {target.ipaddress}</li>
+                                    <li>Target ID - {target.id}</li>
+                                    <li>Engagement ID - {target.engagement_id}</li>
+                                    <li>System Info - {target.sysinfo}</li>
+                                    <li>Vulnerabilities - {target.vulns}</li>
+                                    <li>Log - {target.log}</li>
+                                    <li>Loot - {target.loot}</li>
+                                    <li>Status - {target.status}</li>
+                                </ul>
+                            </li>
+                        )}
+                    </ul>
+                </li> 
+            )}
+        </ul>
     </div>
-
   )
 }
 
