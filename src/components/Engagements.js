5fc4d584ecdada38e672b660541c9958636a892a
mcrooks65 <mfcrooks@gmail.com>
2021-05-21 15:45:22 -0700

    Add hasTargets() to Engagements.js

--- src/components/Engagements.js
+++ src/components/Engagements.js
@@ -1,37 +1,42 @@
 import React from 'react'
 
 const Engagements = (props) => {
-    console.log(props)
   return (
-    
     <div>
         <h2>Engagements Listing</h2>
         <ul>
             {props.engagements.map(engagement => 
                 <li key={engagement.id}>
-                    <h3>#{engagement.id}. {engagement.name} - {engagement.status} </h3>
-                    <ul> 
-                        {engagement.targets.map(target => 
-                            <li key={target.id}>
-                                Hostname - {target.hostname}
-                                <ul>
-                                    <li>IP - {target.ipaddress}</li>
-                                    <li>Target ID - {target.id}</li>
-                                    <li>Engagement ID - {target.engagement_id}</li>
-                                    <li>System Info - {target.sysinfo}</li>
-                                    <li>Vulnerabilities - {target.vulns}</li>
-                                    <li>Log - {target.log}</li>
-                                    <li>Loot - {target.loot}</li>
-                                    <li>Status - {target.status}</li>
-                                </ul>
-                            </li>
-                        )}
-                    </ul>
+                    <h3>#{engagement.id}. {engagement.name} - {engagement.status} </h3> 
+                    {hasTargets(engagement)}
                 </li> 
             )}
         </ul>
     </div>
   )
+}
+
+function hasTargets(engagement) {
+    
+    if (engagement.targets) {
+        return <ul> 
+            {engagement.targets.map(target => 
+                <li key={target.id}>
+                    Hostname - {target.hostname}
+                    <ul>
+                        <li>IP - {target.ipaddress}</li>
+                        <li>Target ID - {target.id}</li>
+                        <li>Engagement ID - {target.engagement_id}</li>
+                        <li>System Info - {target.sysinfo}</li>
+                        <li>Vulnerabilities - {target.vulns}</li>
+                        <li>Log - {target.log}</li>
+                        <li>Loot - {target.loot}</li>
+                        <li>Status - {target.status}</li>
+                    </ul>
+                </li>
+            )}
+        </ul>
+    } 
 }
 
 export default Engagements
