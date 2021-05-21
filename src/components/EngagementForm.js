f8e95efe010031dc56cd226159f281d2ab0fa87e
mcrooks65 <mfcrooks@gmail.com>
2021-05-21 15:48:33 -0700

    Update EngagementForm.js

--- src/components/EngagementForm.js
+++ src/components/EngagementForm.js
@@ -1,13 +1,43 @@
 import React from 'react';
+import {connect} from 'react-redux'
+import {addEngagement} from '../actions/addEngagement'
 
 class EngagementForm extends React.Component {
+
+    state = {
+        name: '',
+        status: ''
+    }
+
+    handleChange = (event)  => {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+        
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault()
+        this.props.addEngagement(this.state)
+    }
+
     render() {
         return (
             <div>
-                EngagementForm
+                <h2>Engagement Form (Under Construction)</h2>
+                <form onSubmit={this.handleSubmit}>
+                    <label>Engagement Name: </label>
+                    <input type='text' placeholder='CTF Name' value={this.state.name} name="name" onChange={this.handleChange}></input>
+                    <br/>
+                    <label>Engagement Status: </label>
+                    <input type='text' placeholder='0/? flags captured!' value={this.state.status} name="status" onChange={this.handleChange}></input>
+                    <br/><br/>
+                    <input type="submit"/>
+                </form>
             </div>
         )
     }
 }
 
-export default EngagementForm
+export default connect(null, {addEngagement})(EngagementForm)
+
