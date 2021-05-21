8694bba7bb4ed98b0fc4b113432f5ad955bc60f1
mcrooks65 <mfcrooks@gmail.com>
2021-05-21 15:56:26 -0700

    Add new case to engagementReducer

--- src/reducers/engagementReducer.js
+++ src/reducers/engagementReducer.js
@@ -3,6 +3,8 @@ export default function engagementReducer(state = {engagements: []}, action) {
     switch (action.type){
         case 'FETCH_ENGAGEMENTS':
             return {engagements: action.payload}
+        case 'ADD_ENGAGEMENT':
+            return {...state, engagements: [...state.engagements, action.payload]}
         default: 
             return state
     }  
