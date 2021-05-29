092b8e19d1748e152a7ea1b48800e7c94d249bb6
mcrooks65 <mfcrooks@gmail.com>
2021-05-28 17:32:51 -0700

    Refactor hasTargets() into Targets.js

--- src/components/Engagement.js
+++ src/components/Engagement.js
@@ -3,48 +3,20 @@ import TargetsContainer from '../containers/TargetsContainer'
 
 const Engagement = (props) => {
     
-    let engagement = props.engagements[props.match.params.id - 1]
-    console.log(engagement)
+   let engagement = props.engagements[props.match.params.id - 1]
+   
+    // console.log(props)
+    // let engagement = props.engagements.filter(engagement => engagement.id == props.match.params.id)
 
-    // To Do: Refactor hasTargets into Target.js component
     return (
         <div>
             <div>
                 <h3>#{engagement ? engagement.id : null}. {engagement ? engagement.name : null} - {engagement ? engagement.status : null} </h3> 
-                {engagement ? hasTargets(engagement) : null} 
                 <TargetsContainer engagement={engagement}/>
             </div>
         </div>
   )
 
-}
-
-function hasTargets(engagement) {
-    
-    if (engagement.targets) {
-        return <ul> 
-            {engagement.targets.map(target => 
-                <li key={target.id}>
-                    Hostname - {target.hostname}
-                    <ul>
-                        <li>IP - {target.ipaddress}</li>
-                        <li>Target ID - {target.id}</li>
-                        <li>Engagement ID - {target.engagement_id}</li>
-                        <li>System Info - {target.sysinfo}</li>
-                        <li>Vulnerabilities - {target.vulns}</li>
-                        <li>Log - {target.log}</li>
-                        <li>Loot - {target.loot}</li>
-                        <li>Status - {target.status}</li>
-                    </ul>
-                </li>
-            )}
-        </ul>
-    } else {
-        // why isn't this line running on page refresh?  works okay when entering new engagement..
-        return <ul><li>No targets added!</li></ul>
-    }
-    
-                   
 }
 
 export default Engagement
--- src/components/Targets.js
+++ src/components/Targets.js
@@ -3,9 +3,33 @@ import React from 'react'
 const Targets = (props) => {
     return (
         <div>
-            Targets
+            <ul> 
+            {props.targets && props.targets.map(target => 
+                <li key={target.id}>
+                    Hostname - {target.hostname}
+                    <ul>
+                        <li>IP - {target.ipaddress}</li>
+                        <li>Target ID - {target.id}</li>
+                        <li>Engagement ID - {target.engagement_id}</li>
+                        <li>System Info - {target.sysinfo}</li>
+                        <li>Vulnerabilities - {target.vulns}</li>
+                        <li>Log - {target.log}</li>
+                        <li>Loot - {target.loot}</li>
+                        <li>Status - {target.status}</li>
+                    </ul>
+                </li>
+            )}
+        </ul>
+
         </div>
     )
 }
+
+// Once again having trouble showing Engagements have no targets!
+// function hasTargets(props) {
+//     if (!props.targets){
+//         return <ul><li>No targets added!</li></ul>
+//     }
+// }
 
 export default Targets
