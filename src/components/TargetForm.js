0f82586c5ca92f4de51931b367aa90ae42f55c35
mcrooks65 <mfcrooks@gmail.com>
2021-05-28 17:33:27 -0700

    Update TargetForm.js

--- src/components/TargetForm.js
+++ src/components/TargetForm.js
@@ -1,13 +1,63 @@
 import React from 'react'
+import {connect} from 'react-redux'
+import {addTarget} from '../actions/addTarget'
 
 class TargetForm extends React.Component {
+
+    state = {
+        ipaddress: '',
+        hostname: '',
+        sysinfo: 'Requires Enumeration!',
+        vulns: 'None found yet, keep enumerating!',
+        log: 'Log activities and enter notes here.',
+        loot: 'Flags, Credentials, Hashes go here.',
+        status: 'Vacant'
+    }
+
+    handleChange = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault()
+        this.props.addTarget(this.state, this.props.engagement.id)
+        this.setState({
+            ipaddress: '',
+            hostname: '',
+            sysinfo: 'Requires Enumeration!',
+            vulns: 'None found yet, keep enumerating!',
+            log: 'Log activities and enter notes here.',
+            loot: 'Flags, Credentials, Hashes go here.',
+            status: 'Vacant'
+        })
+    }
+
     render() {
         return (
             <div>
-                TargetForm
+                <h2>Add New Target to this Engagement</h2>
+                <form onSubmit={this.handleSubmit}>
+                    <label>Hostname: </label>
+                    <input type="text" name="hostname" value={this.state.hostname} onChange={this.handleChange}></input>
+                    <br/>
+                    <label>IP Address: </label>
+                    <input type="text" name="ipaddress" value={this.state.ipaddress} onChange={this.handleChange}></input>
+                    <br/>
+                    {/* <label>Status: </label>
+                    <select name="status" value={this.state.status} onChange={this.handleChange}>
+                        <option>Vacant</option>
+                        <option>Shelled</option>
+                        <option>Rooted</option>
+                    </select> */}
+                    <br/><br/>
+                    <input type="submit"/>
+                </form>
+                
             </div>
         )
     }
 }
 
-export default TargetForm
+export default connect(null, {addTarget})(TargetForm)
