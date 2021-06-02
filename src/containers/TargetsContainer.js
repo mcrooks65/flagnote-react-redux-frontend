a659eeb4418fbf9db48f78d89ce1c8e82e8e30e7
mcrooks65 <mfcrooks@gmail.com>
2021-06-02 15:42:53 -0700

    Add Switch/Route wrapping and Target.js component

--- src/containers/TargetsContainer.js
+++ src/containers/TargetsContainer.js
@@ -1,16 +1,37 @@
 import React from 'react'
+import {connect} from 'react-redux';
 import TargetForm from '../components/TargetForm'
 import Targets from '../components/Targets'
+import Target from '../components/Target'
+import {Route, Switch} from 'react-router-dom';
 
 class TargetsContainer extends React.Component {
+
     render() {
         return (
             <div>
-                <TargetForm engagement={this.props.engagement}/>
-                <Targets targets={this.props.engagement && this.props.engagement.targets}/>
+                <Switch>
+                    <Route exact path='/engagements/:engagement_id/targets/:target_id' render={(routerProps) => <Target {...routerProps} targets={this.props.engagement && this.props.engagement.targets}/>}/> 
+                    <Route path = '/engagements/:engagement_id'>
+                        <Targets targets={this.props.engagement && this.props.engagement.targets}/>
+                        <TargetForm engagement={this.props.engagement}/>
+                    </Route>
+                </Switch>            
             </div>
         )
     }
 }
 
-export default TargetsContainer
+// TODO: TRY AND GET THIS WORKING - READ THE DOCS!  Do we neccessarily even need this nested mapStatetoProps?
+const mapStateToProps = (state, ownProps) => {
+    console.log(state)
+    console.log(ownProps)
+    console.log(ownProps.engagement)
+    // console.log(ownProps.engagement.id)
+    // console.log(state.engagements[ownProps.engagement.id].targets)
+    return {
+        targets: state.targets // ownProps.engagement.targets only works on second invocation
+    }
+}
+
+export default connect(mapStateToProps)(TargetsContainer)
