aca8c26d001132aa7db605f4ce5ab34015a2b0c7
mcrooks65 <mfcrooks@gmail.com>
2021-06-06 14:49:10 -0700

    Cleanup Engagement.js component

--- src/components/Engagement.js
+++ src/components/Engagement.js
@@ -2,21 +2,18 @@ import React from 'react'
 import TargetsContainer from '../containers/TargetsContainer'
 
 const Engagement = (props) => {
-    
-   let engagement = props.engagements[props.match.params.id - 1]
-   
-    // console.log(props)
-    // let engagement = props.engagements.filter(engagement => engagement.id == props.match.params.id)
+
+    let engagementID = props.match.params.id - 1
+    let engagement = props.engagements[engagementID]
 
     return (
         <div>
             <div>
-                <h3>#{engagement ? engagement.id : null}. {engagement ? engagement.name : null} - {engagement ? engagement.status : null} </h3> 
+                <h3>#{engagement ? engagement.id : null}. {engagement ? engagement.name : null} - {engagement ? engagement.status : null} </h3>            
                 <TargetsContainer engagement={engagement}/>
             </div>
         </div>
-  )
-
+    )
 }
 
 export default Engagement
