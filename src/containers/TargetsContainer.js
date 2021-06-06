eef40ffa1ab112110887c4cfe802c1b4c1ccb418
mcrooks65 <mfcrooks@gmail.com>
2021-06-06 14:59:29 -0700

    Cleanup TargetsContainer

--- src/containers/TargetsContainer.js
+++ src/containers/TargetsContainer.js
@@ -2,7 +2,7 @@ import React from 'react'
 import {connect} from 'react-redux';
 import TargetForm from '../components/TargetForm'
 import Targets from '../components/Targets'
-import Target from '../components/Target'
+// import Target from '../components/Target'
 import {Route, Switch} from 'react-router-dom';
 
 class TargetsContainer extends React.Component {
@@ -11,27 +11,15 @@ class TargetsContainer extends React.Component {
         return (
             <div>
                 <Switch>
-                    <Route exact path='/engagements/:engagement_id/targets/:target_id' render={(routerProps) => <Target {...routerProps} targets={this.props.engagement && this.props.engagement.targets}/>}/> 
+                    {/* <Route exact path='/engagements/:engagement_id/targets/:target_id' render={(routerProps) => <Target {...routerProps} targets={this.props.engagement && this.props.engagement.targets}/>}/>  */}
                     <Route path = '/engagements/:engagement_id'>
                         <Targets targets={this.props.engagement && this.props.engagement.targets}/>
                         <TargetForm engagement={this.props.engagement}/>
                     </Route>
-                </Switch>            
+                </Switch>
             </div>
         )
     }
 }
 
-// TODO: TRY AND GET THIS WORKING - READ THE DOCS!  Do we neccessarily even need this nested mapStatetoProps?
-const mapStateToProps = (state, ownProps) => {
-    console.log(state)
-    console.log(ownProps)
-    console.log(ownProps.engagement)
-    // console.log(ownProps.engagement.id)
-    // console.log(state.engagements[ownProps.engagement.id].targets)
-    return {
-        targets: state.targets // ownProps.engagement.targets only works on second invocation
-    }
-}
-
-export default connect(mapStateToProps)(TargetsContainer)
+export default connect(null)(TargetsContainer)
