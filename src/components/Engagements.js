d17575e308a06b7201dafd46b8542a03bc68408a
mcrooks65 <mfcrooks@gmail.com>
2021-07-14 17:06:59 -0700

    Add sort button to Engagements component

--- src/components/Engagements.js
+++ src/components/Engagements.js
@@ -1,20 +1,45 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
-// import Engagement from './Engagement'
+// import {connect} from 'react-redux'
+import Engagement from './Engagement'
 
-const Engagements = (props) => {
-  return (
-    <div class="bg-red-700 font-mono p-4">
-        <h2 class="text-center text-black text-5xl font-bold p-10">Engagements Listing</h2>
-        <div class="text-2xl font-bold italic underline">
-            {props.engagements.map(engagement => 
-                <li class="p-1"key={engagement.id}>
-                    <Link to={`/engagements/${engagement.id}`}>{engagement.name}: {engagement.status}</Link>
-                </li> 
-            )}
-        </div>
-    </div>
-  )
+
+class Engagements extends React.Component {
+
+  state = {
+    sortedEngagements: [...this.props.engagements],
+    sorted: false
+  }
+
+  sortEngagements = () => {
+    this.state.sorted = !this.state.sorted
+    console.log(this.state.sorted)
+    if (this.state.sorted === true) {
+      this.setState({sortedEngagements: this.state.sortedEngagements.sort((a,b) => (a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0))})
+    } else {
+      this.setState({sortedEngagements: [...this.props.engagements]})
+    }
+    console.log(this.state.sortedEngagements)
+  }
+
+ 
+
+  render() {
+    return ( 
+      <div class="container font-mono p-4">
+          <h2 class="text-center text-black text-5xl font-bold p-10">Engagements Listing</h2>
+          <button class="bg-green-700 p-2" onClick={() => this.sortEngagements()} value='Sort Button'>Sort Button</button>
+          <div class="text-2xl font-bold italic underline">
+              {this.state.sortedEngagements.map(engagement => 
+                  <li class="p-1"key={engagement.id}>
+                      <Link to={`/engagements/${engagement.id}`}>{engagement.name}: {engagement.status}</Link>
+                  </li> 
+              )}
+          </div>
+      </div>
+
+    )
+  }
 }
-
+  
 export default Engagements
